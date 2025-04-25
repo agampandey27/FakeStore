1f7d8952d524bee2b20f2829ceea8afc3ef3ad4e
Agam Pandey <agampandey705@gmail.com>
2025-04-25 12:59:56 +0530

    Nav code Cleared

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,3 @@
-// import React from 'react'
-// import {Link} from 'react-router-dom'
-// import { FaShoppingCart } from 'react-icons/fa'
-
-// const Navbar = () => {
-//   return (
-//       <nav className='sticky top-0 z-50 flex items-center justify-between px-7 py-4 bg-[#111]'>
-        
-//         <Link to='/' className='text-3xl font-bold flex'><img className='h-8 m-1' src="https://fakestoreapi.com/icons/logo.png" alt="" />FakeStore</Link>
-            
-//         <div className='flex gap-8'>
-//             <Link to='/' className='text-lg transform transition duration-300 hover:scale-110 hover:text-blue-400'>Home</Link>
-//             <Link to='/my-cart' className='text-lg transform transition duration-300 hover:scale-110 hover:text-blue-400 flex items-center justify-center gap-1'>My Cart <FaShoppingCart/></Link>
-//             <Link to='/about' className='text-lg transform transition duration-300 hover:scale-110 hover:text-blue-400'>About</Link>
-//             <Link to='/contact' className='text-lg transform transition duration-300 hover:scale-110 hover:text-blue-400'>Contact</Link>
-//         </div>
-//       </nav>
-//   )
-// }
-
-// export default Navbar
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
@@ -52,7 +30,6 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile Links Dropdown */}
       {isOpen && (
         <div className="md:hidden mt-3 flex flex-col gap-4 text-white px-2">
           <Link to="/" className="hover:text-blue-400 transition" onClick={() => setIsOpen(false)}>Home</Link>
